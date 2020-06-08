ce5e3d5dbd7edbfce5e0467c1245de51a9cc217a
Shilpa Muralidhar <basavannashilpa@gmail.com>
2020-06-08 07:35:02 -0500

    filepath changes

--- TOP_SD.py
+++ TOP_SD.py
@@ -2,5 +2,5 @@ import SearchDrugs as SD
 SeaDrugs = SD.SD()
 import pickle
 
-with open("/Users/shilpa/Desktop/Appa/homeopath-webapp/SD.pkl", "wb") as file:
+with open("/Users/shilpa/Desktop/Appa/homeopathwebapp/SD.pkl", "wb") as file:
 	pickle.dump(SeaDrugs,file) 
