d6ca98c1e98b44768194e10b10e3f44dfdafd960
Shilpa Muralidhar <basavannashilpa@gmail.com>
2020-11-16 13:43:31 -0600

    bootstrap

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with open(f'SD.pkl', 'rb') as f:
     loaded_object = pickle.load(f)
    
 
-app = flask.Flask(__name__, template_folder='templates')
+app = flask.Flask(__name__, template_folder='template_1')
 
 # Set up the main route
 @app.route('/', methods=['GET', 'POST'])
@@ -23,7 +23,7 @@ def main():
 
     if flask.request.method == 'GET':
         # Just render the initial form, to get input
-        return(flask.render_template('main.html'))
+        return(flask.render_template('index.html'))
 
     if flask.request.method == 'POST':
        # Extract the input
@@ -39,11 +39,11 @@ def main():
 
     # Render the form again, but add in the prediction and remind the user
     # of the values they input before
-        return flask.render_template('main.html',
+        return flask.render_template('index.html',
                                 original_input={'Symptoms': retVal},
                                                 result=prediction)   
     #else:
-        #return render_template('main.html')
+        #return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
